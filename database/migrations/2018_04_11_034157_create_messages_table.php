18762cd9080d353b27397647428477a5d5fc503c
vmventura <vfventura@up.edu.ph>
2018-04-11 22:14:02 +0800

    added database migrate for chat and messages

---
+++ database/migrations/2018_04_11_034157_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('chatID');
+            $table->integer('senderID');
+            $table->text('message');
+            $table->dateTime('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
