712dbf96588a8be4b6ed2ea437c742fe67ad39d2
vmventura <vfventura@up.edu.ph>
2018-04-13 19:56:17 +0800

    added model for Chat

---
+++ app/Chat.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Chat extends Model
+{
+		public function chatMates(){	
+			return $this->belongsTo('App\Student', 'chatMate');
+		}
+
+		public function scopeGetChatMate($query, $userID){
+			$user1 = Chat::select('id', 'user1 as chatMate', 'updated_at')->where('user2', $userID)->get();
+			$user2 = Chat::select('id', 'user2 as chatMate', 'updated_at')->where('user1', $userID)->get();
+			return $user1->merge($user2)->sortByDesc('date');
+
+
+		}
+    protected $table = 'chat';
+}
---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Chat;
+use App\Student;
+use Auth;
+use DB;
+
+class ChatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $user = Auth::id();
+        $user=1;
+        // $chat = Chat::
+                    
+        //                 where('user2', $user)
+        //                 ->orWhere('user1', $user)
+        //                 ->get();
+        // print_r($chat);
+
+        // $chat = DB::select(`student.name`,`chat.id`,`chat.date`)
+        // ->from(`chat`)
+        // ->from(`student`)
+        // ->where(DB::raw(`( chat.user1 = $user OR chat.user2 = $user )` ))
+        // ->where(DB::raw(`( ( (chat.user1 = student.id) or (chat.user2 = student.id) ) AND student.id != $user )` ))
+        // ->orderBy(`date`, `DESC`)
+        // ->orderBy(`student.name`, `ASC`)
+        // ->get();
+
+        // $chat = DB::
+        // from(`chat`)
+        // ->from(`student`)
+        // // ->where(DB::raw(`( chat.user1 = $user OR chat.user2 = $user )` ))
+        // ->get();
+
+        // echo "<pre>";
+        // print_r($chat);
+        // return view('chat', compact('chat'));
+
+        $chat= Chat::GetChatMate($user);
+        return view('chat', compact('chat'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
