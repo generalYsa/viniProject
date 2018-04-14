896d64f8c7a5cb4e690ce95bc303781ab914a0b5
vmventura <vfventura@up.edu.ph>
2018-04-15 01:12:01 +0800

    edited DB schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('IDnum')->unique(); //student/profNum
             $table->string('password');
+            $table->string('userType'); //student / prof
             $table->rememberToken();
             $table->timestamps();
         });
