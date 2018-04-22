3177f646f3a47a09e70a01ffa3f98fd62c1a0323
vmventura <vfventura@up.edu.ph>
2018-04-22 17:42:49 +0800

    Added Model for Tables

---
+++ app/Events.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+use DB;
+use Illuminate\Database\Eloquent\Model;
+
+class Events extends Model
+{
+    protected $table = 'events';
+    protected $fillable = ['classID', 'author', 'name', 'description', 'date'];
+
+	// this specifies that author is an ID of table User
+	public function authorTable(){	
+		return $this->belongsTo('App\User', 'author');
+	}
+
+	// this specifies that author is an ID of table Class
+	public function classIDTable(){	
+		return $this->belongsTo('App\Class', 'classID');
+	}
+
+	// this returns all Events under Class with id $id
+	public function scopeGetEvents($query, $id){	
+		return Events::	select('id', 'name', 'author', 'description', 'date', 'updated_at',  DB::raw('"event" as type')/*, author*/)
+							->where('classID', $id)
+							->get()->sortByDesc('updated_at');
+	}
+}
