aadf514eb04dc32a89ae4faa5228b0b3ac2bb282
vmventura <vfventura@up.edu.ph>
2018-04-22 17:43:26 +0800

    edited for timeline

--- routes/web.php
+++ routes/web.php
@@ -12,89 +12,6 @@
 */
 
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// Route::get('/timeline', function () {
-//     return view('timeline');
-// });
-// Route::get('/chat', function () {
-//     return view('chat');
-// });
-// Route::get('/studySets', function () {
-//     return view('studySets');
-// });
-// Route::get('/learnStudySet', function () {
-//     return view('learnStudySet');
-// });
-// Route::get('/playStudySet', function () {
-//     return view('playStudySet');
-// });
-// Route::get('/editStudySet', function () {
-//     return view('editStudySet');
-// });
-// Route::get('/recordForm', function() {
-// 	return view('recordForm');
-// });
-
-// Route::get('/requirements', function() {
-// 	return view('requirements');
-// });
-
-// Route::get('/viewFiles', function() {
-// 	return view('viewFiles');
-// });
-
-// Route::get('/calendar', function() {
-// 	return view('calendar');
-// });
-
-// Route::get('/studentList', function() {
-// 	return view('studentList');
-// });
-
-// Route::get('/editPicture', function() {
-// 	return view('editPicture');
-// });
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/studySets', function () {
-//     return view('studySets');
-// });
-// Route::get('/recordForm', function() {
-// 	return view('recordForm');
-
-// });
-
-// Route::get('/requirements', function() {
-// 	return view('requirements');
-// });
-
-// Route::get('/viewFiles', function() {
-// 	return view('viewFiles');
-// });
-
-// Route::get('/calendar', function() {
-// 	return view('calendar');
-// });
-
-// Route::get('/studentList', function() {
-// 	return view('studentList');
-// });
-
-// Route::get('/editPicture', function() {
-// 	return view('editPicture');
-// });
-
-// Auth::routes();
-// Route::get('/home', 'HomeController@index')->name('home');
-// Route::post('/home', 'ChatControl@store')->name('insertMessage');
-// Route::resource('/chat', 'ChatController');
-
-
 Auth::routes();
 
 Route::group(['middleware' => ['auth']], function() {
@@ -150,16 +67,21 @@ Route::group(['middleware' => ['auth']], function() {
 	});
 	
 
-	Route::get('/timeline', function() {
-		return view('timeline');
-	});
+	// Route::get('/timeline', function() {
+	// 	return view('timeline');
+	// });
 	
 		// Route::get('/welcome', function() {
 		// 	return view('layouts.app');
 		// });
 
 	Route::get('/chat/getSentMessage', 'ChatController@index');
-	});
+
+	Route::post('/timeline/savePost', 'TimelineController@savePost');
+	Route::post('/timeline/saveEvent', 'TimelineController@saveEvent');
+	Route::post('/timeline/saveActivity', 'TimelineController@saveActivity');
+	Route::resource('/timeline', 'TimelineController');
+});
 
 Route::get('/chat/getSentMessage', 'ChatController@getSentMessage');
 Route::get('/', '\App\Http\Controllers\Auth\LoginController@showLoginForm');
