15e9b4dae8054425518c323e658ab3127b4710d8
Kent Fernandez Rio <kfrio@up.edu.ph>
2018-04-29 10:39:53 +0800

    Added Submissions Table Seeder

---
+++ database/seeds/SubmissionTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Submission;
+
+class SubmissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Submission::create([
+			'id' => '',
+			'activityID' => '1',
+			'studentID' => '201510558',
+			'score' => '5',
+			'fileID' => '1',
+		]);
+		
+		Submission::create([
+			'id' => '',
+			'activityID' => '1',
+			'studentID' => '201510559',
+			'score' => '5',
+			'fileID' => '2',
+		]);
+    }
+}
