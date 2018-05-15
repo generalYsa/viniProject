6d20bee4177aba86e41747d7f8f6c5a6ab204cb1
vmventura <vfventura@up.edu.ph>
2018-05-15 19:36:23 +0800

    Created Migration for userActivity

---
+++ database/migrations/2018_05_13_144721_create_user_activity_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserActivityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('userActivity', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('userID');
+            $table->integer('activityID');
+            $table->integer('score')->default(0);
+            $table->boolean('isPost');
+            $table->boolean('viewed')->default(false);
+            $table->boolean('isDone')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_activity');
+    }
+}
