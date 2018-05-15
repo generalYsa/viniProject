04b0f4b44e273b0e7ac78562bfb58af92734dd9f
Kent Fernandez Rio <kfrio@up.edu.ph>
2018-05-15 16:15:29 +0800

    Changed Activity Tabel Seeder values

--- database/seeds/ActivityTableSeeder.php
+++ database/seeds/ActivityTableSeeder.php
@@ -15,11 +15,31 @@ class ActivityTableSeeder extends Seeder
         Activity::create([
             'name'  => 'Assignment 1',
             'description' => 'This is our first assignment.',
-            'score' => '100',
+            'score' => 100,
             'classID'  => '1',
 			'author' => '201312345',
 			'gradeReqID' => '1',
-			'deadline' => '2018-04-28 09:29:36',
+			'deadline' => '2018-06-28 09:29:36',
+        ]);
+		
+		Activity::create([
+            'name'  => 'Problem Set 1',
+            'description' => 'This is our first problem set.',
+            'score' => 100,
+            'classID'  => '1',
+			'author' => '201312345',
+			'gradeReqID' => '4',
+			'deadline' => '2018-06-28 09:29:36',
+        ]);
+		
+		Activity::create([
+            'name'  => 'Lab Activity 1',
+            'description' => 'This is our first lab activity.',
+            'score' => 100,
+            'classID'  => '1',
+			'author' => '201312345',
+			'gradeReqID' => '3',
+			'deadline' => '2018-06-28 09:29:36',
         ]);
     }
 }
