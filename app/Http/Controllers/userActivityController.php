750891bce2597b3f83857cab5c0f38787d7f8114
vmventura <vfventura@up.edu.ph>
2018-05-16 16:08:28 +0800

    Creates userActivity Controller

--- app/Http/Controllers/userActivityController.php
+++ app/Http/Controllers/userActivityController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\userActivity;
 
 class userActivityController extends Controller
 {
-    
+
+	// sets all ToDos of user as viewed. Called when ToDo button is clicked
+    public function SetViewed(){
+    	userActivity::SetView();
+    }
+
+    public function getLastUserActivity(Request $request){
+    	return userActivity::GetLastUserActivity();
+    }
 }
