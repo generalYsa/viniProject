dcb81fa0595f746384b7a26b91c54ceb2e123256
sshrbnc <38273738+sshrbnc@users.noreply.github.com>
2018-05-18 23:50:20 +0800

    edited Classes model

--- app/Classes.php
+++ app/Classes.php
@@ -8,14 +8,18 @@ class Classes extends Model
 {
 
 	public function scopeGetClasses($query, $id){
-    	return Classes::where('profID', $id)->get();
+    	return Classes::where('profNum', $id)->get();
     }
 
     public function scopeSelectClass($query, $id){
     	return Classes::where('id', $id)->get();
     }
 
+    public function scopeGetClassID($query, $code){
+    	return Classes::where('code', $code)->get();
+    }
+
 	protected $fillable = ['name', 'profID', 'code'];
-    protected $table = 'classes';
+    protected $table = 'class';
     public $timestamps = false;
 }
